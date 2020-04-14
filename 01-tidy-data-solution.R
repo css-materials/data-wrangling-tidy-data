8b038cd4f78713abcd6c8ba3017265dc770ef678
Benjamin Soltoff <ben@bensoltoff.com>
2020-04-14 10:58:50 -0500

    Add starter files

---
+++ 01-tidy-data-solution.R
@@ -0,0 +1,50 @@
+# load required packages
+library(tidyverse)
+library(rcfss)
+
+# tidy `race`
+race
+
+## tidying operation
+pivot_longer(
+  data = race,
+  cols = -Name,
+  names_to = "Time",
+  values_to = "Score",
+  # ensure the Time column is stored as a numeric column
+  names_ptypes = list(Time = double())
+)
+
+# tidy `results`
+results
+
+## tidying operation
+pivot_wider(data = results, names_from = Treatment, values_from = value)
+
+# tidy `grades`
+grades
+
+## tidying operation
+pivot_longer(
+  data = grades,
+  cols = c(Fall:Winter),
+  names_to = "Quarter",
+  values_to = "Score"
+) %>%
+  pivot_wider(
+    names_from = Test,
+    values_from = Score
+  )
+
+# tidy `activities`
+activities
+
+## tidying operation
+pivot_longer(
+  data = activities,
+  cols = c(work.T1:talk.T2),
+  names_to = "variable",
+  values_to = "value"
+) %>%
+  separate(variable, into = c("location", "time")) %>%
+  pivot_wider(names_from = location, values_from = value)
---
+++ 01-tidy-data.R
@@ -0,0 +1,27 @@
+# load required packages
+library(tidyverse)
+library(rcfss)
+
+# tidy `race`
+race
+
+## tidying operation
+
+
+# tidy `results`
+results
+
+## tidying operation
+
+
+# tidy `grades`
+grades
+
+## tidying operation
+
+
+# tidy `activities`
+activities
+
+## tidying operation
+
